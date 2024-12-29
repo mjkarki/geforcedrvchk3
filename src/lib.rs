a6007a36d6e060b50aad9c7845662a7c3c8a8102
Matti J. Karki <mjk@iki.fi>
2024-12-29 20:35:26 +0200

    Apply Rust analyzer changes

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,12 @@
 //! The page this library is using for fetching information is this:
 //! <https://www.geforce.com/drivers>
 
-use std::{env, path::Path, path::PathBuf, process::Command};
-use regex::Regex;
 use json;
+use regex::Regex;
 use reqwest::blocking;
+use std::{env, path::Path, path::PathBuf, process::Command};
 
-use std::io::Write;             // Just for flush()
+use std::io::Write; // Just for flush()
 use std::io::{stdin, stdout};
 
 pub const VERSION: &str = "0.5.1";
@@ -28,7 +28,9 @@ const NVIDIA_URL: &str = r"https://gfwsl.geforce.com/services_toolkit/services/c
 pub fn get_page(url: &str) -> Result<String, &'static str> {
     let response = blocking::get(url);
     match response {
-        Ok(resp) => resp.text().or(Err("The page has invalid UTF-8 characters!")),
+        Ok(resp) => resp
+            .text()
+            .or(Err("The page has invalid UTF-8 characters!")),
         Err(_) => Err("Unable to access the online resources!"),
     }
 }
@@ -42,13 +44,19 @@ pub fn get_page(url: &str) -> Result<String, &'static str> {
 ///
 /// If the information cannot be retrieved, then an error message is provided
 /// as a result.
-pub fn get_available_version_information(get_page: fn (&str) -> Result<String, &'static str>) -> Result<(String, String), &'static str> {
+pub fn get_available_version_information(
+    get_page: fn(&str) -> Result<String, &'static str>,
+) -> Result<(String, String), &'static str> {
     let page = get_page(NVIDIA_URL)?;
     let data = json::parse(&page).or(Err("Incorrect information at the online resource!"))?;
     let json_version = &data["IDS"][0]["downloadInfo"]["Version"];
     let json_url = &data["IDS"][0]["downloadInfo"]["DownloadURL"];
-    let version = json_version.as_str().ok_or("Cannot find version information from the online resource!")?;
-    let url = json_url.as_str().ok_or("Cannot find download URL information from the online resource!")?;
+    let version = json_version
+        .as_str()
+        .ok_or("Cannot find version information from the online resource!")?;
+    let url = json_url
+        .as_str()
+        .ok_or("Cannot find download URL information from the online resource!")?;
     Ok((version.to_string(), url.to_string()))
 }
 
@@ -60,32 +68,38 @@ pub fn get_available_version_information(get_page: fn (&str) -> Result<String, &
 /// found), then an error message is provided as a result.
 pub fn get_installed_version(executable_name: &str) -> Result<String, &'static str> {
     let nvidiasmi = get_nvidia_smi_location(&executable_name)?;
-    let output = Command::new(nvidiasmi).output().or(Err("Couldn't detect installed version. Maybe the driver is not installed?"))?;
+    let output = Command::new(nvidiasmi).output().or(Err(
+        "Couldn't detect installed version. Maybe the driver is not installed?",
+    ))?;
     let pattern = Regex::new(r"Driver Version: ([0-9]+\.[0-9]+)").unwrap();
     let nvsmi = String::from_utf8_lossy(&output.stdout);
-    let captures = pattern.captures(&nvsmi).ok_or("Cannot find installed version information!")?;
+    let captures = pattern
+        .captures(&nvsmi)
+        .ok_or("Cannot find installed version information!")?;
     Ok(String::from(&captures[1]))
 }
 
 /// Find nvidia-smi.exe and return full path.
 fn get_nvidia_smi_location(executable_name: &str) -> Result<String, &'static str> {
-    let nvidia_smi_path_old: PathBuf = ["NVIDIA Corporation", "NVSMI", &executable_name].iter().collect();
+    let nvidia_smi_path_old: PathBuf = ["NVIDIA Corporation", "NVSMI", &executable_name]
+        .iter()
+        .collect();
     let nvidia_smi_path_new: PathBuf = ["System32", &executable_name].iter().collect();
     let mut nvidiasmi = PathBuf::new();
     nvidiasmi.push(env::var("windir").expect("Environment variable 'windir' not found!"));
     nvidiasmi.extend(&nvidia_smi_path_new);
     if Path::new(&nvidiasmi).exists() == false {
         let mut nvidiasmi = PathBuf::new();
-        nvidiasmi.push(env::var("ProgramFiles").expect("Environment variable 'ProgramFiles' not found!"));
+        nvidiasmi.push(
+            env::var("ProgramFiles").expect("Environment variable 'ProgramFiles' not found!"),
+        );
         nvidiasmi.extend(&nvidia_smi_path_old);
         if nvidiasmi.exists() == false {
             Err("Couldn't detect location for nvidia-smi. Maybe the driver is not installed?")
-        }
-        else {
+        } else {
             Ok(String::from(nvidiasmi.to_string_lossy()))
         }
-    }
-    else {
+    } else {
         Ok(String::from(nvidiasmi.to_string_lossy()))
     }
 }
@@ -95,7 +109,12 @@ fn get_nvidia_smi_location(executable_name: &str) -> Result<String, &'static str
 /// command-line and the URL is not sanitized in any way. It's possible to run
 /// arbitrary commands with this function.
 pub fn start_browser(url: &str) {
-    Command::new(env::var("ComSpec").expect("Environment variable 'ComSpec' not found!")).arg("/c").arg("start").arg(url).spawn().unwrap();
+    Command::new(env::var("ComSpec").expect("Environment variable 'ComSpec' not found!"))
+        .arg("/c")
+        .arg("start")
+        .arg(url)
+        .spawn()
+        .unwrap();
 }
 
 /// Asks message from user and lists options. The default option is zero-based
@@ -133,9 +152,10 @@ pub fn ask_confirmation(message: &str, options: &[char], default: usize) -> usiz
 
         if input.trim().len() == 0 {
             break default;
-        }
-        else {
-            let pos = &options.iter().position(|&x| x.to_lowercase().next() == input.chars().next().unwrap().to_lowercase().next());
+        } else {
+            let pos = &options.iter().position(|&x| {
+                x.to_lowercase().next() == input.chars().next().unwrap().to_lowercase().next()
+            });
             match pos {
                 Some(value) => break *value,
                 None => input.clear(),
@@ -178,19 +198,28 @@ mod tests {
     /// Test that fetching available driver data works.
     #[test]
     fn get_available_version_information_success() {
-        assert_eq!(get_available_version_information(get_test_page).is_ok(), true);
+        assert_eq!(
+            get_available_version_information(get_test_page).is_ok(),
+            true
+        );
     }
 
     /// Test that fetching available driver version works.
     #[test]
     fn get_available_version_number_success() {
-        assert_eq!(get_available_version_information(get_test_page).unwrap().0, "123.45");
+        assert_eq!(
+            get_available_version_information(get_test_page).unwrap().0,
+            "123.45"
+        );
     }
 
     /// Test that fetching available driver URL works.
     #[test]
     fn get_available_version_url_success() {
-        assert_eq!(get_available_version_information(get_test_page).unwrap().1, "https://example.com/test.exe");
+        assert_eq!(
+            get_available_version_information(get_test_page).unwrap().1,
+            "https://example.com/test.exe"
+        );
     }
 
     /// Stub function for unit tests. Imitates get_page() function.
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,7 @@
-use geforcedrvchk3::{get_available_version_information,
-                     get_page,
-                     get_installed_version,
-                     start_browser,
-                     ask_confirmation,
-                     VERSION,
-                     SMI};
+use geforcedrvchk3::{
+    ask_confirmation, get_available_version_information, get_installed_version, get_page,
+    start_browser, SMI, VERSION,
+};
 use std::io::{stdin, stdout, Write};
 
 fn handle_error<T>(result: Result<T, &'static str>) -> T {
@@ -18,7 +15,7 @@ fn handle_error<T>(result: Result<T, &'static str>) -> T {
             stdout().flush().unwrap();
             stdin().read_line(&mut input).unwrap();
             std::process::exit(1);
-        },
+        }
     }
 }
 
@@ -28,17 +25,30 @@ fn main() {
     let installed: String = handle_error(get_installed_version(SMI));
     let available: (String, String) = handle_error(get_available_version_information(get_page));
 
-    let instd_ver: f64 = handle_error(installed.parse().or(Err("Cannot convert installed version number!")));
-    let avail_ver: f64 = handle_error(available.0.parse().or(Err("Cannot convert available version number!")));
+    let instd_ver: f64 = handle_error(
+        installed
+            .parse()
+            .or(Err("Cannot convert installed version number!")),
+    );
+    let avail_ver: f64 = handle_error(
+        available
+            .0
+            .parse()
+            .or(Err("Cannot convert available version number!")),
+    );
     let avail_url: String = available.1;
 
     println!("Currently installed driver version: {instd_ver}");
 
     if instd_ver < avail_ver {
         println!("New driver version is available:    {avail_ver}\n");
-        match ask_confirmation("Do you want to \
+        match ask_confirmation(
+            "Do you want to \
                                 (d)ownload the latest driver, or \
-                                (q)uit?", &vec!['d', 'q'], 0) {
+                                (q)uit?",
+            &vec!['d', 'q'],
+            0,
+        ) {
             0 => start_browser(&avail_url),
             _ => (),
         }
